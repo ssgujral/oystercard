04c752dfda13ddc1077f002163e7792cff7a32aa
samgujral <71089547+samgujral@users.noreply.github.com>
2021-12-25 23:18:45 +0400

    Challenge 7 Completed

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,6 +2,8 @@ class Oystercard
  
   attr_reader :balance
 
+  TOP_UP_LIMIT = 90
+
   def initialize()
     
     @balance = 0
@@ -10,9 +12,16 @@ class Oystercard
 
   def top_up(top_up_amount)
   
+    raise Exception.new("Your Oystercard balance cannot exceed £#{TOP_UP_LIMIT}.") if @balance + top_up_amount > TOP_UP_LIMIT 
+
     @balance = @balance + top_up_amount
     return @balance
 
+  end
+
+  def deduct(deduction_amount)
+    @balance = @balance - deduction_amount
+    return @balance
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -7,7 +7,7 @@ describe Oystercard do
   it { is_expected.to respond_to(:balance) }
 
   describe "#balance" do
-    it "should return 0 balance for a new card" do
+    it "returns 0 balance for new card" do
       expect(card.balance).to eq(0)
     end
   end 
@@ -15,14 +15,24 @@ describe Oystercard do
   it { is_expected.to respond_to(:top_up).with(1).argument }
 
   describe "#top_up" do 
-    it "should top the card by the amount entered" do
+    it "tops up the card by the amount entered" do
       starting_balance = card.balance
       card.top_up(5)
       expect(card.balance).to eq(starting_balance + 5)
     end
+
+    it "gives error if user tries to top up beyond the maximum balance" do
+      top_up_limit = Oystercard::TOP_UP_LIMIT
+      subject.top_up top_up_limit
+      expect { subject.top_up 1 }.to raise_error("Your Oystercard balance cannot exceed £#{top_up_limit}.")
+    end
+  end
+
+  describe "#deduct" do
+    it "deducts the amount entered from the user's Oystercard balance" do
+      pre_deduction_balance = card.balance
+      card.deduct(5)
+      expect(card.balance).to eq(pre_deduction_balance - 5)
+    end  
   end
 end
-
-#new_card = Oystercard.new
-#expect(new_card.check_balance).to eq 0
-
